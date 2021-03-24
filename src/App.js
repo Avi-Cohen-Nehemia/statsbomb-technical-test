a1b8c698eb2c68c47024ff5ef68c7e7a30a297e2
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-24 13:31:21 +0000

    import the players table into app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import PlayersTable from "./components/PlayersTable";
 
 const App = () => {
   return (
-    <>
+    <Container>
       <h1>StatsBomb</h1>
-    </>
+      <PlayersTable />
+    </Container>
   );
 }
 
