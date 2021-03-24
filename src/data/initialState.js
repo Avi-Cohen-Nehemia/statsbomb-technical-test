5076deafee087757cb5c15ebb514fe66c18219d2
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-24 11:13:09 +0000

    import all the data from the mock db and pass it to the initial state

--- src/data/initialState.js
+++ src/data/initialState.js
@@ -1,8 +1,14 @@
+// import all the data from the mock db and pass it to the initial state
+const players = require('./database/player-data.json');
+const teams = require('./database/team-data.json');
+const matches = require('./database/match-data.json');
+const stats = require('./database/stat-data.json');
+
 const initialState = {
-	players: [],
-    teams: [],
-    matches: [],
-    stats: []
+	players: players,
+    teams: teams,
+    matches: matches,
+    stats: stats
 };
 
 export default initialState;
