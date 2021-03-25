c39441fdba89722c41cb53bd36a8c2426a9e4eef
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-25 09:56:15 +0000

    create a fucntion that calculates useful totals info by team

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,34 +1,32 @@
-const playersData = require("./../data/database/player-data.json");
-const statsData = require("./../data/database/stat-data.json");
-
 export const getStatsByPlayer = (players, stats) => {
 
-    let playersDetails = players.map((player) => {
+    const playersDetails = players.map((player) => {
         return {
             player_id: player.player_id,
             player_name: player.player_known_name ? player.player_known_name : player.player_name
         }
     });
 
-    let playersListWithStats = playersDetails.map((player) => {
+    const playersListWithStats = playersDetails.map((player) => {
 
-        let playerStats = stats.filter((stat) => {
+        // filter to get only relevant stats
+        const playerStats = stats.filter((stat) => {
             return stat.player_id === player.player_id;
         });
 
-        let total_goals = playerStats.reduce((acc, stat) => {
+        const total_goals = playerStats.reduce((acc, stat) => {
             return acc + stat.goals;
         }, 0);
 
-        let total_shots = playerStats.reduce((acc, stat) => {
+        const total_shots = playerStats.reduce((acc, stat) => {
             return acc + stat.shots;
         }, 0);
 
-        let total_completed_passes = playerStats.reduce((acc, stat) => {
+        const total_completed_passes = playerStats.reduce((acc, stat) => {
             return acc + stat.completed_passes;
         }, 0);
 
-        let average_team_possession_percentage = playerStats.reduce((acc, stat) => {
+        const average_team_possession_percentage = playerStats.reduce((acc, stat) => {
             return acc + stat.team_possession_percentage
 
         // add conditional to avoid getting NaN when length is 0
@@ -44,4 +42,67 @@ export const getStatsByPlayer = (players, stats) => {
     })
 
     return playersListWithStats;
+};
+
+export const getStatsByTeam = (teams, matches) => {
+
+    const teamsListWithStats = teams.map(({ team_id }, index) => {
+
+        // filter to get only the matches where the team played at
+        const teamMatches = matches.filter((match) => {
+            return  team_id === match.match_home_team_id || team_id === match.match_away_team_id;
+        });
+
+
+        const total_goals = teamMatches.reduce((acc, match) => {
+
+            if (team_id === match.match_home_team_id) {
+                return acc + match.match_home_score;
+            }
+
+            return acc + match.match_away_score;
+        }, 0);
+
+
+        const total_wins = teamMatches.reduce((acc, {
+            match_home_score,
+            match_away_score,
+            match_home_team_id,
+            match_away_team_id,
+            match_home_penalty_score,
+            match_away_penalty_score
+        }) => {
+
+            // when match DID NOT go to penalties
+            if (match_home_score !== match_away_score) {
+
+                if (team_id === match_home_team_id && match_home_score > match_away_score) {
+                    return acc + 1;
+                }
+    
+                if (team_id === match_away_team_id && match_away_score > match_home_score) {
+                    return acc + 1;
+                }
+            }
+
+            // when match DID go to penalties
+            if (team_id === match_home_team_id && match_home_penalty_score > match_away_penalty_score) {
+                return acc + 1;
+            }
+
+            if (team_id === match_away_team_id && match_away_penalty_score > match_home_penalty_score) {
+                return acc + 1;
+            }
+
+            return acc;
+        }, 0);
+
+        return {
+            ...teams[index],
+            total_goals: total_goals,
+            total_wins: total_wins
+        }
+    })
+
+    return teamsListWithStats;
 };
