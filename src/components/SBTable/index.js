4fb44d62fcc3e52ac595352a2c930f32bbdf88f2
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-25 11:12:56 +0000

    create a method that filters and sorts out the data

--- src/components/SBTable/index.js
+++ src/components/SBTable/index.js
@@ -3,9 +3,30 @@ import Table from 'react-bootstrap/Table'
 
 class SBTable extends Component {
 
+    constructor(props) {
+
+        super(props);
+
+        this.getTopResults = this.getTopResults.bind(this);
+    }
+
+    // sort and filter results by selected amount and column
+    getTopResults(data, amount, column) {
+
+        const sortedData = data.sort((dataPointA, dataPointB) => {
+            return dataPointB[column] - dataPointA[column];
+        });
+
+        const topResults = sortedData.filter((_, index) => {
+            return index < amount;
+        })
+
+        return topResults;
+    }
+
     render() {
 
-        const { tableHeaders, tableData } = this.props
+        const { tableHeaders, tableData, top, by } = this.props
 
         return(
             <Table striped bordered hover variant="dark">
@@ -13,12 +34,12 @@ class SBTable extends Component {
                     <tr>
                         <th>#</th>
                         { tableHeaders.map((header, index) => (
-                            <th key={ index }>{ header }</th>
+                            <th key={ index }>{ header.replace(/_/g, ' ') }</th>
                         )) }
                     </tr>
                 </thead>
                 <tbody>
-                    { tableData.map((dataPoint, index) => (
+                    { this.getTopResults(tableData, top, by).map((dataPoint, index) => (
                         <tr key={ index }>
                             <td>{index + 1}</td>
                             { tableHeaders.map((header) => (
