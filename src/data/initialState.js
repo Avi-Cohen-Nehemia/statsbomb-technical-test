13b6bcb16ad64f1737e516622ed13d9bc62702d5
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-25 10:32:32 +0000

    add calculated stats by player and team to intialState

--- src/data/initialState.js
+++ src/data/initialState.js
@@ -3,12 +3,15 @@ import players from "./database/player-data";
 import teams from "./database/team-data";
 import matches from "./database/match-data";
 import stats from "./database/stat-data";
+import { getStatsByPlayer, getStatsByTeam } from "./../utility";
 
 export const initialState = {
 	players: players,
     teams: teams,
     matches: matches,
-    stats: stats
+    stats: stats,
+    statsByPlayer: getStatsByPlayer(players, stats),
+    statsByTeam: getStatsByTeam(teams, matches)
 };
 
 export default initialState;
