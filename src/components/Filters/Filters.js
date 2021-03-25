0618de4d8326030d856a22ec3600e8f82244edd0
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-25 18:16:19 +0000

    create wrapper file for Filters

---
+++ src/components/Filters/Filters.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+
+class Filters extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            displayBy: props.selectedColumn,
+            amountToDisplay: props.selectedAmount,
+            forTable: props.forTable
+        }
+
+        this.handleDisplayBy = this.handleDisplayBy.bind(this);
+        this.handleAmountToDisplay = this.handleAmountToDisplay.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleDisplayBy(column) {
+        this.setState({ displayBy: column.replace(/ /g, '_') });
+    }
+
+    handleAmountToDisplay(amount) {
+        this.setState({ amountToDisplay: amount });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.props.updateFilters(this.state);
+    }
+
+    render() {
+
+        const { columns } = this.props
+        const amounts = [5, 10, 20];
+
+        return(
+            <Form className="d-flex mb-2" onSubmit={ this.handleSubmit }>
+                <DropdownButton
+                    title={ this.state.displayBy.replace(/_/g, ' ') }
+                >
+                    {columns.map((column, index) => (
+                        <Dropdown.Item
+                            key={index}
+                            onClick={() => this.handleDisplayBy(column)}
+                        >
+                            { column.replace(/_/g, ' ') }
+                        </Dropdown.Item>
+                    ))}
+                </DropdownButton>
+                <DropdownButton
+                    className="ml-3"
+                    title={ `Top: ${this.state.amountToDisplay}` }
+                >
+                    {amounts.map((amount, index) => (
+                        <Dropdown.Item
+                            key={index}
+                            onClick={() => this.handleAmountToDisplay(amount)}
+                        >
+                            { amount }
+                        </Dropdown.Item>
+                    ))}
+                </DropdownButton>
+                <Button
+                    className="ml-3"
+                    type="submit"
+                >
+                    {"APPLY"}
+                </Button>
+            </Form>
+        )
+    }
+}
+
+export default Filters;
--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,73 +1,11 @@
-import React, { Component } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
+import { connect } from "react-redux";
+import Filters from "./Filters";
+import { updateFilters } from "./../../data/actions/state";
 
-class Filters extends Component {
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateFilters: (data) => dispatch(updateFilters(data)),
+    };
+};
 
-    constructor(props) {
-
-        super(props);
-
-        this.displayBy = this.displayBy.bind(this);
-        this.amountToDisplay = this.amountToDisplay.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    displayBy() {
-
-    }
-
-    amountToDisplay() {
-
-    }
-
-    handleSubmit() {
-
-    }
-
-    render() {
-
-        const { options, selectedOption, selectedAmount } = this.props
-        const amounts = [5, 10, 20];
-
-        return(
-            <Form className="d-flex mb-2" onSubmit={ this.handleSubmit }>
-                <DropdownButton
-                    title={ selectedOption.replace(/_/g, ' ') }
-                >
-                    {options.map((option, index) => (
-                        <Dropdown.Item
-                            key={index}
-                            onClick={() => this.displayBy(option)}
-                        >
-                            { option.replace(/_/g, ' ') }
-                        </Dropdown.Item>
-                    ))}
-                </DropdownButton>
-                <DropdownButton
-                    className="ml-3"
-                    title={ selectedAmount }
-                >
-                    {amounts.map((amount, index) => (
-                        <Dropdown.Item
-                            key={index}
-                            onClick={() => this.amountToDisplay(amount)}
-                        >
-                            { amount }
-                        </Dropdown.Item>
-                    ))}
-                </DropdownButton>
-                <Button
-                    className="ml-3"
-                    type="submit"
-                >
-                    {"APPLY"}
-                </Button>
-            </Form>
-        )
-    }
-}
-
-export default Filters;
+export default connect(null, mapDispatchToProps)(Filters);
