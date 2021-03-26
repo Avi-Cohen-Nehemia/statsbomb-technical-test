cb26691aa793da6b5ba9826f42c020ba62de40db
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-26 11:54:17 +0000

    import ComparisonChart into App

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./assets/css/main.css";
 import Container from "react-bootstrap/Container";
 import TopPlayersTable from "./components/TopPlayersTable";
 import TopTeamsTable from "./components/TopTeamsTable";
+import ComparisonChart from "./components/ComparisonChart";
 
 const App = () => {
   return (
     <Container>
         <h1 className="display-3">StatsBomb</h1>
-        <section className="d-flex justify-content-between">
+        <section className="d-flex justify-content-between mt-5">
             <TopPlayersTable />
             <TopTeamsTable />
+        </section>
+        <section style={{ height: "450px" }} className="my-4">
+          <ComparisonChart />
         </section>
     </Container>
   );
