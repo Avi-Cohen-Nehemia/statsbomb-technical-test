5894bcaa82199b2213f217d5e05b8bae062d0a90
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-27 13:12:26 +0000

    create another dropdown button to add or remove stats from the graph

--- src/components/ComparisonChart/ComparisonFilter/index.js
+++ src/components/ComparisonChart/ComparisonFilter/index.js
@@ -17,24 +17,44 @@ class ComparisonFilter extends Component {
         this.props.selectTeam(team);
     }
 
+    handleSelectStat(stat) {
+        this.props.selectStat(stat);
+    }
+
     render() {
 
         const { allTeams } = this.props
+        const allStats = ["total_wins", "total_goals"];
 
         return(
-            <DropdownButton
-                className="text-capitalize"
-                title="Teams"
-            >
-                {allTeams.map((team, index) => (
-                    <Dropdown.Item
-                        key={ index }
-                        onClick={ () => this.handleSelectTeam(team) }
-                    >
-                        { team.team_name }
-                    </Dropdown.Item>
-                ))}
-            </DropdownButton>
+            <>
+                <DropdownButton
+                    className="text-capitalize"
+                    title="Teams"
+                >
+                    {allTeams.map((team, index) => (
+                        <Dropdown.Item
+                            key={ index }
+                            onClick={ () => this.handleSelectTeam(team) }
+                        >
+                            { team.team_name }
+                        </Dropdown.Item>
+                    ))}
+                </DropdownButton>
+                <DropdownButton
+                    className="text-capitalize"
+                    title="Stats"
+                >
+                    {allStats.map((stat, index) => (
+                        <Dropdown.Item
+                            key={ index }
+                            onClick={ () => this.handleSelectStat(stat) }
+                        >
+                            { stat }
+                        </Dropdown.Item>
+                    ))}
+                </DropdownButton>
+            </>
         )
     }
 }
