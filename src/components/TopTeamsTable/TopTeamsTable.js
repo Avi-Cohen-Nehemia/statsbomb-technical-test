7c5c2c3108a00564d12659ed0d18a9e903a49fef
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-28 12:46:09 +0100

    change TopTeamsTable to a functional component and add prop types

--- src/components/TopTeamsTable/TopTeamsTable.js
+++ src/components/TopTeamsTable/TopTeamsTable.js
@@ -1,33 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import SBTable from "./../SBTable";
 import Filters from "./../Filters";
 
-class TopTeamsTable extends Component {
+const TopTeamsTable = ({ data, top, by }) => {
 
-    render() {
+    const headers = ["team_name", "total_goals", "total_wins"];
+    const columns = ["total_wins", "total_goals"];
 
-        const { data, top, by } = this.props;
-        const headers = ["team_name", "total_goals", "total_wins"];
-        const columns = ["total_wins", "total_goals"];
+    return(
+        <div style={{ width: "49%" }}>
+            <h2>{ "Top Teams" }</h2>
+            <Filters
+                columns={ columns }
+                forTable={ "statsByTeam" }
+                selectedAmount={ top }
+                selectedColumn={ by }
+            />
+            <SBTable
+                tableHeaders={ headers }
+                tableData={ data }
+                top={ top }
+                by={ by }
+            />
+        </div>
+    )
+}
 
-        return(
-            <div style={{ width: "49%" }}>
-                <h2>{ "Top Teams" }</h2>
-                <Filters
-                    columns={ columns }
-                    forTable={ "statsByTeam" }
-                    selectedAmount={ top }
-                    selectedColumn={ by }
-                />
-                <SBTable
-                    tableHeaders={ headers }
-                    tableData={ data }
-                    top={ top }
-                    by={ by }
-                />
-            </div>
-        )
-    }
+TopTeamsTable.propTypes = {
+    data: PropTypes.array.isRequired,
+    top: PropTypes.number.isRequired,
+    by: PropTypes.string.isRequired,
 }
 
 export default TopTeamsTable;
